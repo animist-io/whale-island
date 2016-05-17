e2496e72381adfde7bd3a5b9770da89609e22b7f
Christopher gewecke <christophergewecke@gmail.com>
2016-05-17 11:20:31 -0700

    Changed to simple node module - added bleno and socket.io

---
+++ test/index.js
@@ -0,0 +1,8 @@
+import assert from 'assert';
+import whaleIsland from '../lib';
+
+describe('whale-island', function () {
+  it('should have unit test!', function () {
+    assert(false, 'we expected this package author to add actual unit tests.');
+  });
+});
