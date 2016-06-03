0e8023edaa7d35df454c611e0c72e30372c8eb03
cgewecke <christophergewecke@gmail.com>
2016-06-02 20:30:01 -0700

    Contract sketch, empty beacon sketch

--- test/index.js
+++ test/index.js
@@ -3,8 +3,6 @@ import whaleIsland from '../lib';
 var chai = require('chai');
 var expect = chai.expect;
 
-console.log(expect);
-
 describe('whale-island', function () {
   it('should have unit test!', function () {
     expect('has a unit test').to.equal(true);
