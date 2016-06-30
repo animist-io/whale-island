079871f616b1bedfabd9251345d2efc02ddfd4d9
cgewecke <christophergewecke@gmail.com>
2016-06-30 15:56:45 -0700

    Utilities units done, found/fixed stackTx bug, found/fixed parseHasTx bug

--- test/server.spec.js
+++ test/server.spec.js
@@ -1,5 +1,6 @@
+let config = require('../lib/config.js');
+
 const server = require('../lib/server.js');
-const config = require('../lib/config.js');
 const account = require('../test/mocks/wallet.js');
 const wallet = require('eth-lightwallet');
 
@@ -9,46 +10,142 @@ const expect = chai.expect;
 
 chai.use(spies);
 
-describe('Characteristic Request Handlers', () => {
+// Set up a keystore and wallet
 
-    var animist, address, keystore, fns = {};
+
+describe('Bluetooth Server', () => {
     
-    // Prep one account for all tests
-    before(() => {
-        let json = JSON.stringify(account.keystore);
-        keystore = wallet.keystore.deserialize(json);
+    var keystore, address;
 
-        
+    // Prep a keystore/account for all tests
+    before(()=>{
+        let json = JSON.stringify(account.keystore);
+        keystore = wallet.keystore.deserialize(json);  
         keystore.generateNewAddress(account.key, 1);
         address = keystore.getAddresses()[0];
-        console.log(address);
-    });
+    })
 
-    // Instantiate a new server before each test
-    beforeEach(() => { 
-        animist = new server.AnimistServer();
-    });
+    describe('Utilites', () => {
 
-    describe('onPinRead', () => {
-      it('should respond to request w/ the current pin', () => {
+        describe('stackTx(tx)', ()=>{
+            var tx, stack, animist, old_config;
+            
+            it('should transform input into buffers of MAX_SIZE & put them on the send stack',()=>{
+                
+                // Testing 11 chars/4 byte packets: 
+                // Stack size should be 3, last element should have length: 3
+                tx = "12341234123";
+                old_config = config.MAX_SEND;
+                config.MAX_SEND = 4;
+                
+                animist = new server.AnimistServer();
+                server.stackTx(tx);
+                stack = animist.getSendStack();
         
-        let codes = config.codes;
-        let pin_to_buffer = new Buffer(animist.getPin());
-        
-        fns.callback = (code, pin) => {};
+                expect(stack.length).to.equal(3);
+                expect(Buffer.isBuffer(stack[0])).to.be.true;
+                expect(stack[2].length).to.equal(3);
 
-        chai.spy.on(fns, 'callback');
-        
-        server.onPinRead(null, fns.callback);
+                // Cleanup 
+                config.MAX_SEND = old_config;
 
-        expect(fns.callback).to.have.been.called.with(codes.RESULT_SUCCESS, pin_to_buffer);
+            });
+        });
 
-      });
+        describe('resetPin', ()=>{
+            var animist, new_pin, old_pin;
+
+            it('should generate & set a new 32 character pin', ()=>{
+                animist = new server.AnimistServer();
+                old_pin = animist.getPin();
+
+                server.resetPin();
+                new_pin = animist.getPin();
+
+                expect( typeof old_pin).to.equal('string');
+                expect( old_pin.length).to.equal(32);
+                expect( typeof new_pin).to.equal('string');
+                expect( new_pin.length).to.equal(32);
+                expect(new_pin).not.to.equal(old_pin);
+            });
+
+        });
+
+        describe('parseHasTxRequest(req)', () =>{
+
+            var req, output, msg;
+
+            it('should return a correctly formatted object representing a signed msg', () =>{
+
+                msg = 'a message';
+                req = wallet.signing.signMsg( keystore, account.key, msg, address); 
+                req = JSON.stringify(req);
+                
+                output = server.parseHasTxRequest(req);
+
+                expect( output.status).to.equal(1);
+                expect( typeof output.val).to.equal('object');
+                expect( Buffer.isBuffer(output.val.r)).to.be.true;
+                expect( Buffer.isBuffer(output.val.s)).to.be.true;
+
+            });
+
+            it('should error correctly if req is not parse-able as a signed msg', ()=>{
+                req = '{\"signed\": \"I am like not even signed LOL!!!\"}';
+                output = server.parseHasTxRequest(req);
+
+                expect(output.status).to.equal(0);
+                expect(output.val).to.equal(config.codes.NO_SIGNED_MSG_IN_REQUEST);
+            });
+
+            it('should error correctly if req is not JSON formatted', ()=>{
+
+                req = "0x5[w,r,0,,n,g";
+                output = server.parseHasTxRequest(req);
+
+                expect(output.status).to.equal(0);
+                expect(output.val).to.equal(config.codes.INVALID_JSON_IN_REQUEST);
+
+            });
+
+        });
+
+
     });
 
-    describe('onHasTxWrite', () => {
-        console.log('address:' + address);
+    describe('Characteristic Request Handlers', () => {
+
+        var animist, fns = {};
+        
+        
+        before(() => {
+            
+        });
+
+        // Instantiate a new server before each test
+        beforeEach(() => { 
+            animist = new server.AnimistServer();
+        });
+
+        describe('onPinRead', () => {
+          it('should respond to request w/ the current pin', () => {
+            
+            let codes = config.codes;
+            let pin_to_buffer = new Buffer(animist.getPin());
+            fns.callback = (code, pin) => {};
+            
+            chai.spy.on(fns, 'callback');
+            server.onPinRead(null, fns.callback);
+
+            expect(fns.callback).to.have.been.called.with(codes.RESULT_SUCCESS, pin_to_buffer);
+
+          });
+        });
+
+        describe('onHasTxWrite', () => {
+            
+
+        });
 
     });
-
 });
