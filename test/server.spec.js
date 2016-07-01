f9d110084682f604101c103db775e881ec61eac5
cgewecke <christophergewecke@gmail.com>
2016-07-01 09:21:08 -0700

    Completed hasTxIndicate Units, stack to queue

--- test/server.spec.js
+++ test/server.spec.js
@@ -10,13 +10,11 @@ const expect = chai.expect;
 
 chai.use(spies);
 
-// Set up a keystore and wallet
-
 describe('Bluetooth Server', () => {
     
     var keystore, address;
 
-    // Prep a keystore/account for all tests
+    // Prep a single keystore/account for all tests
     before(()=>{
         let json = JSON.stringify(account.keystore);
         keystore = wallet.keystore.deserialize(json);  
@@ -26,10 +24,10 @@ describe('Bluetooth Server', () => {
 
     describe('Utilites', () => {
 
-        describe('stackTx(tx)', ()=>{
-            var tx, stack, animist, old_config;
+        describe('queueTx(tx)', ()=>{
+            var tx, queue, animist, old_config;
             
-            it('should transform input into buffers of MAX_SIZE & put them on the send stack',()=>{
+            it('should transform input into buffers of MAX_SIZE & put them in the send queue',()=>{
                 
                 // Testing 11 chars/4 byte packets: 
                 tx = "12341234123";
@@ -37,12 +35,12 @@ describe('Bluetooth Server', () => {
                 config.MAX_SEND = 4;
                 
                 animist = new server.AnimistServer();
-                server.stackTx(tx);
-                stack = animist.getSendStack();
+                server.queueTx(tx);
+                queue = animist.getSendQueue();
         
-                expect(stack.length).to.equal(3);
-                expect(Buffer.isBuffer(stack[0])).to.be.true;
-                expect(stack[2].length).to.equal(3);
+                expect(queue.length).to.equal(3);
+                expect(Buffer.isBuffer(queue[0])).to.be.true;
+                expect(queue[2].length).to.equal(3);
 
                 // Cleanup 
                 config.MAX_SEND = old_config;
@@ -88,7 +86,7 @@ describe('Bluetooth Server', () => {
 
             });
 
-            it('should error correctly if req is not parse-able as a signed msg', ()=>{
+            it('should return error if req is not parse-able as a signed msg', ()=>{
                 req = '{\"signed\": \"I am like not even signed LOL!!!\"}';
                 output = server.parseHasTxRequest(req);
 
@@ -96,7 +94,7 @@ describe('Bluetooth Server', () => {
                 expect(output.val).to.equal(config.codes.NO_SIGNED_MSG_IN_REQUEST);
             });
 
-            it('should error correctly if req is not JSON formatted', ()=>{
+            it('should return error if req is not JSON formatted', ()=>{
 
                 req = "0x5[w,r,0,,n,g";
                 output = server.parseHasTxRequest(req);
@@ -109,7 +107,7 @@ describe('Bluetooth Server', () => {
         });
     });
 
-    describe('Characteristic Request Handlers', () => {
+    describe('Request Handlers', () => {
 
         var animist; 
  
@@ -152,13 +150,12 @@ describe('Bluetooth Server', () => {
 
                  // Mock address
                 config.fakeTx.authority = address;
-
-                
+ 
             });
 
             // Clear state & mock updateValueCallback
             beforeEach(()=>{
-                animist.resetSendStack();
+                animist.resetSendQueue();
                 animist.hasTxCharacteristic.updateValueCallback = (val) => {};
             })
 
@@ -173,44 +170,45 @@ describe('Bluetooth Server', () => {
 
             });
 
-            it('should push the tx onto the send stack', (done) => {
+            it('should push the tx into the send queue', (done) => {
 
-                let initial_stack_size, new_stack_size;
+                let initial_queue_size, new_queue_size;
 
-                initial_stack_size = animist.getSendStack().length;
+                initial_queue_size = animist.getSendQueue().length;
                 config.fakeTx.authority = address;
             
                 server.onHasTxWrite(req, null, null, fns.callback)
-                new_stack_size = animist.getSendStack().length;
+                new_queue_size = animist.getSendQueue().length;
 
-                expect(initial_stack_size).to.equal(0);
-                expect(new_stack_size).to.be.gt(0);
+                expect(initial_queue_size).to.equal(0);
+                expect(new_queue_size).to.be.gt(0);
                 setTimeout(done, 50);
 
             });
 
-            it('should begin writing/processing the send stack', (done) => {
+            it('should begin writing/processing the send queue', (done) => {
 
-                let tx, full_stack, full_stack_size, new_stack_size, expected_stack_size;
+                let tx, full_queue, full_queue_size, new_queue_size, expected_queue_size;
                 
-                // Get a stack copy
+                // Get a queue copy
                 tx = server.getTx(address);
-                server.stackTx(tx);
-                full_stack = animist.getSendStack();
-                full_stack_size = full_stack.length;
-                expected_stack_size = full_stack_size - 1;
+                server.queueTx(tx);
+                full_queue = animist.getSendQueue();
+                full_queue_size = full_queue.length;
+                expected_queue_size = full_queue_size - 1;
 
                 // Clean up
-                animist.resetSendStack();
+                animist.resetSendQueue();
                
-                // Test
+                // Run
                 chai.spy.on(animist.hasTxCharacteristic, 'updateValueCallback');
                 server.onHasTxWrite(req, null, null, fns.callback);
 
+                // Test
                 setTimeout(() => {
-                    new_stack_size = animist.getSendStack().length;
-                    expect(animist.hasTxCharacteristic.updateValueCallback).to.have.been.called.with(full_stack[0]);
-                    expect(new_stack_size).to.equal(expected_stack_size);
+                    new_queue_size = animist.getSendQueue().length;
+                    expect(animist.hasTxCharacteristic.updateValueCallback).to.have.been.called.with(full_queue[0]);
+                    expect(new_queue_size).to.equal(expected_queue_size);
                     done();
                 }, 50);
                 
@@ -226,7 +224,7 @@ describe('Bluetooth Server', () => {
 
             });
 
-            it('should respond w/ correct error code if req is un-parseable', ()=>{
+            it('should respond w/ error code if req is un-parseable', ()=>{
 
                 req = "0x5[w,r,0,,n,g";
                 chai.spy.on(fns, 'callback');
@@ -238,5 +236,83 @@ describe('Bluetooth Server', () => {
 
         });
 
+        describe('onHasTxIndicate', ()=>{
+            var req, fns = {};
+
+            // Mocks
+            before(()=>{
+                
+                // Mock request
+                req = wallet.signing.signMsg( keystore, account.key, animist.getPin(), address); 
+                req = JSON.stringify(req);
+                
+                // Mock byte callback
+                fns.callback = (code) => {};
+
+                 // Mock address
+                config.fakeTx.authority = address;
+ 
+            });
+
+            // Run hasTxWrite: Clear state & mock updateValueCallback
+            beforeEach((done)=>{
+                animist.resetSendQueue();
+                animist.hasTxCharacteristic.updateValueCallback = (val) => {};
+                server.onHasTxWrite(req, null, null, fns.callback);
+                setTimeout(done, 50);
+            });
+
+            it('should de-queue & send the next packet', (done)=>{
+                
+                let queue = animist.getSendQueue();
+                let initial_queue_size = queue.length;
+                let initial_queue_element = queue[0];
+
+                chai.spy.on(animist.hasTxCharacteristic, 'updateValueCallback');
+                server.onHasTxIndicate();
+
+                setTimeout(()=>{
+                    queue = animist.getSendQueue();
+                    expect(animist.hasTxCharacteristic.updateValueCallback).to.have.been.called.with(initial_queue_element);
+                    expect(queue.length).to.equal(initial_queue_size - 1);
+                    done();
+                },10);
+
+            });
+
+            it('should send EOF signal if queue is empty', (done)=>{
+
+                let expected = new Buffer(config.codes.EOF);
+                chai.spy.on(animist.hasTxCharacteristic, 'updateValueCallback');
+
+                animist.resetSendQueue();
+                server.onHasTxIndicate();
+
+                setTimeout(()=>{
+                    expect(animist.hasTxCharacteristic.updateValueCallback).to.have.been.called.with(expected);
+                    done();
+                },10);
+
+            });
+
+            it('should do nothing post-EOF', (done)=>{
+
+                // Run EOF
+                animist.resetSendQueue();
+                server.onHasTxIndicate();
+
+                setTimeout(()=>{
+                    
+                    // Post EOF
+                    chai.spy.on(animist.hasTxCharacteristic, 'updateValueCallback');
+                    server.onHasTxIndicate();
+
+                    setTimeout(() =>{
+                        expect(animist.hasTxCharacteristic.updateValueCallback).not.to.have.been.called();
+                        done();
+                     },0)
+                },0);
+            });
+        });
     });
 });
