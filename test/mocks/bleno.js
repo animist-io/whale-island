817dfd1edbb652d12c9f4a48076d66d7a71a2b2e
cgewecke <christophergewecke@gmail.com>
2016-10-03 20:29:48 -0700

    Issue #35: iBeacon broadcaster

--- test/mocks/bleno.js
+++ test/mocks/bleno.js
@@ -4,6 +4,7 @@
 module.exports.Characteristic = class Characteristic{ constructor(obj){} };
 module.exports.PrimaryService = class PrimaryService{ constructor(obj){} };
 module.exports.startAdvertising = (val, array, fn) => {};
+module.exports.startAdvertisingIBeacon = (uuid, major, minor, power, fn) => {};
 module.exports.setServices = (array) => {};
 module.exports.disconnect = () => {};
 module.exports.on = (event, fn) => {};
